43bbf9030df3b189539d285ca338d39bbea306d9
Talles <talves2503@gmail.com>
2024-10-24 22:54:13 -0300

    feat: setup assets and colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,23 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  theme: {
+    extend: {
+      colors: {
+        dark: "#0b0b0b", 
+        second_dark: "#161616",     
+        light: "##F4F4F5",
+        second_light: "#D8D8D8",     
+        primary: "#005670",
+        bd_pending: "#CE9400",
+        bd_preparing: "#7631BB",
+        bd_prepared: "#250EB0",
+        bd_in_route: "#00A9CE",
+        bd_completed: "#00BB07",
+        bd_canceled: "#A60C0C",
+      }
+    }
+  },
   plugins: [],
 };
 export default config;
